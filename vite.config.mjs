b00007d43c4dbb6726bf205162ccc7219a9becc5
Shaun van Tonder <shaunmichaelvantonder@gmail.com>
2024-07-04 22:38:40 +0200

    yarn/vite

---
+++ vite.config.mjs
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [
+    laravel({
+      input: 'resources/js/app.js',
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+        compilerOptions: {
+          isCustomElement: tag => ['select-box', 'help'].includes(tag),
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'resources/js'),
+    },
+  },
+});
